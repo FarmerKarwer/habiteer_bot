fc89690419373dadd1a24dcdeceb7978e94cbfa1
FarmerKarwer <dlectus@mail.ru>
2024-10-30 11:41:09 +0300

    Minor changes

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 import json
-import bot_logic
+from bot_logic import use_logic
 import tg_methods
 
 def handler(event, context):
 	#Change it to json.loads(event['body']) when using it on Yandex Cloud
 	message = tg_methods.get_updates()['result'][-1] 
 	print(message)
-	bot_logic.use_logic(message)	
+	use_logic(message)	
 	return {
 	'statusCode':200,
 	'body':event
